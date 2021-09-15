0f1b73d0842ea276694d48afa29df6a822bd0d0f
Quadrillion <89971311+Quadrillion1024@users.noreply.github.com>
2021-09-15 22:08:10 +0800

    第三版本
    设计了几个样例效果不好，追加了另一种余弦相似度度的计算但是本质上无事发生。
    本来想换一个计算方式直到看到了舍友的海明距离发现也不太行所以放弃。
    明天进行格式整理和异常处理之后大概就是最终版本了罢！
    修正了一部分表达和命名规范，以及输出结果位数的Bug！
    🌭🌭🌭🍚🍚🍚🍜🍜🍜

--- main.py
+++ main.py
@@ -6,7 +6,6 @@
 
 # 3219005446 姜珺杨19信息安全1班
 # 编译软件用的个人习惯的jupyter notebook，有分段运行所以可能导致commit的版本比较诡异
-#在第二版本为了方便命令行操作已经转去加入spyder啦
 # 软件工程个人项目 论文查重
 
 
@@ -15,11 +14,11 @@
 import jieba
 import gensim
 import sys
+import numpy as np
 import re
 import os
 
 # In[2]:
-
 
 # # 测试版本的自用文件路径
 
@@ -27,16 +26,12 @@ import os
 # s2 = r'C:\Users\Quadrillion\Desktop\01\测试文本2\orig_0.8_dis_15.txt'
 # stopwords=[]
 
-# In[3]:
 # 利用jieba分词，将词分好并保存到向量中
-
 # s1_cut = [i for i in jieba.cut(s1, cut_all=True) if (i not in stopwords) and i!='']
 # s2_cut = [i for i in jieba.cut(s2, cut_all=True) if (i not in stopwords) and i!='']
 # word_set = set(s1_cut).union(set(s2_cut))
 
-
 # In[4]:
-
 
 #非测试版本的获取文件路径
 #其实是命令行版本
@@ -50,24 +45,31 @@ def get_file_contents(path):
     f.close()
     return string
 
-
 # In[3]:
-
 
 #将读取到的文件内容先把标点符号、转义符号等特殊符号过滤掉，然后再进行结巴分词
 #jieba.luct生成list
 def filter(string):
     pattern = re.compile(u"[^a-zA-Z0-9\u4e00-\u9fa5]")
     string = pattern.sub("", string)
-    result = jieba.lcut(string)
-    return result
- 
+    '''
+    result_text = jieba.lcut(string)
+    '''
+    return string
 
-
+# In[3]:
+    '''
+def filter(string):
+	result_text = []
+	pattern = re.compile(u"[^a-zA-Z0-9\u4e00-\u9fa5]")   #定义正则表达式匹配模式
+	data = pattern.sub("",string)  # 只保留英文a-zA-z、数字0-9和中文\u4e00-\u9fa5的结果。	去除标点符号
+	result_text = [i for i in jieba.cut(data, cut_all=False) if i != '']  #分词
+	return result_text
+    '''
 # In[6]:
-
-
-#传入过滤数据,余弦相似度 1
+#待优化的余弦相似度
+#传入过滤数据
+'''
 def calc_similarity(text1, text2):
     texts = [text1, text2]
     dictionary = gensim.corpora.Dictionary(texts)
@@ -76,10 +78,50 @@ def calc_similarity(text1, text2):
     test_corpus_1 = dictionary.doc2bow(text1)
     cosine_sim = similarity[test_corpus_1][1]
     return cosine_sim
- 
+'''
+# In[]
+def get_word_vector(s1,s2):
+    """
+    :param s1: 句子1
+    :param s2: 句子2
+    :return: 返回句子的余弦相似度
+    """
+    # 分词
+    cut1 = jieba.cut(s1)
+    cut2 = jieba.cut(s2)
+    list_word1 = (','.join(cut1)).split(',')
+    list_word2 = (','.join(cut2)).split(',')
+
+    # 列出所有的词,取并集
+    key_word = list(set(list_word1 + list_word2))
+    # 给定形状和类型的用0填充的矩阵存储向量
+    word_vector1 = np.zeros(len(key_word))
+    word_vector2 = np.zeros(len(key_word))
+
+    # 计算词频
+    # 依次确定向量的每个位置的值
+    for i in range(len(key_word)):
+        # 遍历key_word中每个词在句子中的出现次数
+        for j in range(len(list_word1)):
+            if key_word[i] == list_word1[j]:
+                word_vector1[i] += 1
+        for k in range(len(list_word2)):
+            if key_word[i] == list_word2[k]:
+                word_vector2[i] += 1
+    return word_vector1, word_vector2
+
+
+# In[]
+def calc_similarity(vec1,vec2):
+    """
+    :param vec1: 向量1
+    :param vec2: 向量2
+    :return: 返回两个向量的余弦相似度
+    """
+    dist1=float(np.dot(vec1,vec2)/(np.linalg.norm(vec1)*np.linalg.norm(vec2)))
+    return dist1
 
 # In[5]:
-
 
 def main_test(text1_abs_path,text2_abs_path):
     
@@ -87,25 +129,21 @@ def main_test(text1_abs_path,text2_abs_path):
     str2 = get_file_contents(text2_abs_path)
     text1 = filter(str1)
     text2 = filter(str2)
-    similarity = calc_similarity(text1, text2)   #生成的similarity变量类型为<class 'numpy.float32'>
-    result=round(similarity.item(),2)  #借助similarity.item()转化为<class 'float'>，然后再取小数点后两位
+    vec1,vec2=get_word_vector(text1,text2)
+    similarity = calc_similarity(vec1, vec2)   
+    result=round(similarity,4) 
+    #四舍五入
     result=result*100
     return result
-
-
-# In[8]:
-
-# print("文本相似度为：%.2f%%"%main_test())
-
 # In[ ]:
 
-
-#命令行版本的获取文件路径 参数传入：main.py、[论文原文的文件的绝对路径]、[抄袭版论文的文件的绝对路径]、[输出的答案文件的绝对路径]
+#命令行版本的获取文件路径
 if __name__ == '__main__':
  
     text1_abs_path = sys.argv[1]
     text2_abs_path = sys.argv[2]
     save_abs_path = sys.argv[3]
+#命令行参数传入：main.py、[论文原文的文件的绝对路径]、[抄袭版论文的文件的绝对路径]、[输出的答案文件的绝对路径]
     result=main_test(text1_abs_path,text2_abs_path)
     
     print("文章相似度： %.2f" % result)
